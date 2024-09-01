c7bf4396a521cd1c518b77da8ff5f44570c8ebea
ezekiel-campos <ezekielcampos@Ezekiels-MacBook-Pro.local>
2024-08-31 23:38:28 -0700

    created all files for the api routes

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 
 const app = express();
 
+const routes = require('./routes')
+
 const db = require('./config/connection');
 
 const PORT = 3001;
@@ -9,6 +11,8 @@ const PORT = 3001;
 app.use(express.urlencoded({ extended: true }));
 
 app.use(express.json());
+
+app.use(routes)
 
 db.once('open', () => {
   app.listen(PORT, () => {
